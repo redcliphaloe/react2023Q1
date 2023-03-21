93264c7e4c3ec562a08151148042a9bbdc6305cb
redcliphaloe <redcliphaloe@gmail.com>
2023-03-21 22:12:52 +0300

    feat: add pages links style

--- components/src/components/PagesLinks.tsx
+++ components/src/components/PagesLinks.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import './PagesLinks.css';
 
 function PagesLinks() {
   return (
-    <>
+    <nav className="pages-links">
       <Link to="/">Home</Link>
       <Link to="/about-us">About us</Link>
-    </>
+    </nav>
   );
 }
 
