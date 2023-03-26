a0d2510a86b319286b24feaf297a6b43f59cc61e
redcliphaloe <redcliphaloe@gmail.com>
2023-03-26 19:21:45 +0300

    feat: change form & add style

--- src/components/FormsForm/index.tsx
+++ src/components/FormsForm/index.tsx
@@ -1,47 +1,57 @@
-function FormsForm() {
-  return (
-    <form>
-      <div>
-        <img src="" alt="photo" />
-        <label htmlFor="file">
-          Upload photo
-          <input type="file" id="file" style={{ display: 'none' }} />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="name">
-          Name:
-          <input type="text" id="name" />
-        </label>
-        <div>
-          <label htmlFor="male">Sex: </label>
-          <input type="radio" name="sex" id="male" />
-          <label htmlFor="male">Male</label>
-          <input type="radio" name="sex" id="female" />
-          <label htmlFor="female">Female</label>
+import React from 'react';
+import './style.css';
+
+class FormsForm extends React.Component {
+  #defaultPhoto = 'url(/src/assets/img/no-image.png)';
+
+  render() {
+    return (
+      <form className="forms-form">
+        <div className="forms-form-photo">
+          <div
+            className="forms-form-photo__img"
+            style={{ backgroundImage: this.#defaultPhoto }}
+          ></div>
+          <label className="forms-form-photo__lbl" htmlFor="file">
+            Upload photo
+            <input type="file" id="file" style={{ display: 'none' }} />
+          </label>
         </div>
-        <label htmlFor="birthDate">
-          Date of birth:
-          <input type="text" id="birthDate" />
+        <div className="forms-form-inputs">
+          <label htmlFor="name">
+            Name:
+            <input type="text" id="name" />
+          </label>
+          <div>
+            <label htmlFor="male">Sex: </label>
+            <input type="radio" name="sex" id="male" />
+            <label htmlFor="male">Male</label>
+            <input type="radio" name="sex" id="female" />
+            <label htmlFor="female">Female</label>
+          </div>
+          <label htmlFor="birthDate">
+            Date of birth:
+            <input type="text" id="birthDate" />
+          </label>
+          <label htmlFor="continent">
+            Continent:
+            <select id="continent">
+              <option value="eurasia">Eurasia</option>
+              <option value="northAmerica">North America</option>
+              <option value="southAmerica">South America</option>
+              <option value="africa">Africa</option>
+              <option value="australia">Australia</option>
+              <option value="antarctica">Antarctica</option>
+            </select>
+          </label>
+        </div>
+        <label htmlFor="agreement">
+          <input type="checkbox" id="agreement" /> I consent to my personal data
         </label>
-        <label htmlFor="continent">
-          Continent:
-          <select id="continent">
-            <option value="eurasia">Eurasia</option>
-            <option value="northAmerica">North America</option>
-            <option value="southAmerica">South America</option>
-            <option value="africa">Africa</option>
-            <option value="australia">Australia</option>
-            <option value="antarctica">Antarctica</option>
-          </select>
-        </label>
-      </div>
-      <label htmlFor="agreement">
-        <input type="checkbox" id="agreement" /> I consent to my personal data
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-  );
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default FormsForm;
