139e3f42d9676f33e0891b845de5c861f8b25251
redcliphaloe <redcliphaloe@gmail.com>
2023-05-01 05:57:06 +0300

    fix: correct app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 describe('App', () => {
   it('First page name is Home', () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    );
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Home');
   });
 });
