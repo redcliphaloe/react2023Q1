5c8adc7cd89ea2f9f0f2a3493f136c527184e65a
redcliphaloe <redcliphaloe@gmail.com>
2023-05-01 05:32:46 +0300

    feat: add ssr

---
+++ src/entry-client.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
